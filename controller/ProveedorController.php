52fa49a3635890e526784588b7091c501943fdaf
Allison Restrepo <allis.restrepo@gmail.com>
2025-05-15 08:55:48 -0500

    proveedor
    Codigo proveedor controller :)

---
+++ controller/ProveedorController.php
@@ -0,0 +1,80 @@
+<?php
+ include_once '../model/proveedor.php';
+ $dependencia = new proveedor();
+
+ //-------------------------------------------------------------------
+ // Funcion para crear 
+ //-------------------------------------------------------------------
+ if ($_POST['funcion']=='crear'){
+     $proveedor->Crear($_POST['nombre']);
+ }
+
+//-------------------------------------------------------------------
+// Funcion para editar
+//-------------------------------------------------------------------
+if ($_POST['funcion']=='editar'){
+    $proveedor->Editar($_POST['id'], $_POST['nombre']);
+}
+
+//-------------------------------------------------------------------
+// Funcion eliminar
+//-------------------------------------------------------------------
+if ($_POST['funcion']=='eliminar'){
+    $proveedor->Eliminar($_POST['id']);        
+}
+
+//-------------------------------------------------------------------
+// Funcion para buscar todos los registros  
+//-------------------------------------------------------------------
+if ($_POST['funcion']=='buscar_todos'){
+    //Variable que almacena la consulta en formato JSON
+    $json=array();
+    //LLamado al modelo
+    $proveedor->BuscarTodos($_POST['dato']);
+    foreach ($proveedor->objetos as $objeto) {
+        $json[]=array(
+                        'id'=>$objeto->id_prove,
+                        'nombre'=>$objeto->nom_prove
+        );
+    }
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
+}
+
+//-------------------------------------------------------------------
+// Funcion para buscar todos los registros DATATABLES
+//-------------------------------------------------------------------
+if ($_POST['funcion']=='listar'){
+    //Variable que almacena la consulta en formato JSON
+    $json=array();
+    //LLamado al modelo
+    $proveedor->BuscarTodos('');
+    foreach ($proveedor->objetos as $objeto) {
+        $json[]=array(
+                        'id'=>$objeto->id_prove,
+                        'nombre'=>$objeto->nom_prove
+        );
+    }
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
+}
+
+//-------------------------------------------------------------------
+// Funcion para buscar un registros  
+//-------------------------------------------------------------------
+if ($_POST['funcion']=='buscar'){
+    //Variable que almacena la consulta en formato JSON
+    $json=array();
+    //LLamado al modelo
+    $proveedor->Buscar($_POST['dato']);
+    foreach ($proveedor->objetos as $objeto) {
+        
+        $json[]=array(
+                        'id'=>$objeto->id_prove,
+                        'nombre'=>$objeto->nom_prove
+        );
+    }
+    $jsonstring = json_encode($json[0]);
+    echo $jsonstring;
+}
+?>
